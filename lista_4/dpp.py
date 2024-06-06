b6a695961a0888c55aa46533dadfb58eb5f30eaf
xywa5000 <xywa5000@gmail.com>
2024-06-06 23:00:42 +0200

    Listy 4
    python code - Dijkstra's solution

--- lista_4/dpp.py
+++ lista_4/dpp.py
@@ -2,55 +2,78 @@ import threading
 import time
 import random
 
-class Philosopher(threading.Thread):
-    def __init__(self, id, left_fork, right_fork, max_meals):
-        threading.Thread.__init__(self)
-        self.id = id
-        self.left_fork = left_fork
-        self.right_fork = right_fork
-        self.max_meals = max_meals
-        self.meals_eaten = 0
+N = 5  # number of philosophers (and forks)
+k = 3  # number of meals each philosopher needs to eat
 
-    def run(self):
-        while self.meals_eaten < self.max_meals:
-            print(f'Philosopher {self.id} is thinking.')
-            time.sleep(random.uniform(1, 3))  # Philosophers think for a while
+class State:
+    THINKING = 0
+    HUNGRY = 1
+    EATING = 2
 
-            # Pick up the forks by hierarchy order
-            first_fork, second_fork = (self.left_fork, self.right_fork) if self.left_fork.id < self.right_fork.id else (self.right_fork, self.left_fork)
-            
-            print(f'Philosopher {self.id} is hungry and tries to pick up fork {first_fork.id}.')
-            with first_fork.lock:
-                print(f'Philosopher {self.id} picked up fork {first_fork.id}.')
-                print(f'Philosopher {self.id} tries to pick up fork {second_fork.id}.')
-                with second_fork.lock:
-                    print(f'Philosopher {self.id} picked up fork {second_fork.id}.')
-                    print(f'Philosopher {self.id} is eating.')
-                    time.sleep(random.uniform(1, 2))  # Philosophers eat for a while
-                    self.meals_eaten += 1
-                    print(f'Philosopher {self.id} has finished eating. Meals eaten: {self.meals_eaten}/{self.max_meals}.')
+def left(i):
+    return (i - 1 + N) % N
 
-            # Forks are released automatically when exiting the `with` block
-            print(f'Philosopher {self.id} put down forks {first_fork.id} and {second_fork.id}.')
+def right(i):
+    return (i + 1) % N
 
-        print(f'Philosopher {self.id} has left the table after eating {self.meals_eaten} meals.')
+state = [State.THINKING] * N
+meal_count = [0] * N  # array to track the number of meals each philosopher has eaten
+critical_region_mtx = threading.Lock()
+output_mtx = threading.Lock()
 
-class Fork:
-    def __init__(self, id):
-        self.id = id
-        self.lock = threading.Lock()
+both_forks_available = [threading.Semaphore(0) for _ in range(N)]
 
-def main():
-    num_philosophers = 30
-    max_meals = 30  # Set the maximum number of meals each philosopher should eat
-    forks = [Fork(i) for i in range(num_philosophers)]
-    philosophers = [Philosopher(i, forks[i], forks[(i + 1) % num_philosophers], max_meals) for i in range(num_philosophers)]
+def my_rand(min_val, max_val):
+    return random.randint(min_val, max_val)
 
-    for philosopher in philosophers:
-        philosopher.start()
+def test(i):
+    if state[i] == State.HUNGRY and state[left(i)] != State.EATING and state[right(i)] != State.EATING:
+        state[i] = State.EATING
+        both_forks_available[i].release()
 
-    for philosopher in philosophers:
-        philosopher.join()
+def think(i):
+    duration = my_rand(400, 800) / 1000.0
+    with output_mtx:
+        print(f"{i} is thinking {duration * 1000:.0f}ms")
+    time.sleep(duration)
+
+def take_forks(i):
+    with critical_region_mtx:
+        state[i] = State.HUNGRY
+        with output_mtx:
+            print(f"\t\t\t{i} is HUNGRY")
+        test(i)
+    both_forks_available[i].acquire()
+
+def eat(i):
+    duration = my_rand(400, 800) / 1000.0
+    with output_mtx:
+        print(f"\t\t\t\t\t\t{i} is eating {duration * 1000:.0f}ms his {meal_count[i]+1}. meal")
+    time.sleep(duration)
+    meal_count[i] += 1
+
+def put_forks(i):
+    with critical_region_mtx:
+        state[i] = State.THINKING
+        test(left(i))
+        test(right(i))
+
+def philosopher(i):
+    while meal_count[i] < k:
+        think(i)
+        take_forks(i)
+        eat(i)
+        put_forks(i)
+    with output_mtx:
+        print(f"\t\t\t\t\t\t\t\t\t\t\tPhilosopher {i} has finished eating {k} meals.")
 
 if __name__ == "__main__":
-    main()
+    print("dp_14")
+
+    threads = [threading.Thread(target=philosopher, args=(i,)) for i in range(N)]
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+    
+    print("All philosophers have eaten their meals.")
